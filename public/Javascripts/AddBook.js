b914f878394ebc57e207e98d1b31a75d33470b0d
Dhruvil-Rangani <dhruvilrangani007@gmail.com>
2023-11-20 17:09:37 -0500

    Frontend_Connected_TO_Database
    Added User's functionality which allows user's from website to add book and delete book  which can be stored in database. In turn, configured server to display and redirect user to homepage and show user added book or delete the book

---
+++ public/Javascripts/AddBook.js
@@ -0,0 +1,42 @@
+// Wait for the DOM to be fully loaded before running the script
+document.addEventListener('DOMContentLoaded', function(){
+    const form = document.getElementById('addBookForm');
+    
+    form.addEventListener('submit', function(event){
+        event.preventDefault();// Prevent the default form submission
+    
+        //Using formData to retrieve data from the form fields
+        const formData = new FormData(form);
+        const data = {
+            // Get the User's input value 
+            title: formData.get('title'),
+            author: formData.get('author'),
+            date: formData.get('date'),
+            category: formData.get('category')
+        };
+
+        // Send a fetch request to the server
+        fetch('/book',{
+            method: 'Post',
+            headers: {
+                'Content-Type': 'application/json' // Informing the server that client is sending Json data 
+            },
+            body: JSON.stringify(data)
+        })
+        .then(response => {
+            // redirecting user to home page after successful book addition into database
+            if(response.ok){
+                window.location.href = '/';
+            } 
+            
+            // If the response is not successful, throw an error
+            throw new Error('Network response was not ok.');
+        })
+        .then(data => {
+            console.log('Success:', data); // Handle success
+        })
+        .catch(error => {
+            console.error('Error: ', error);// Handle error
+        })
+    })
+})
---
+++ public/Javascripts/DeleteBook.js
@@ -0,0 +1,31 @@
+// Wait for the DOM to be fully loaded before running the script
+document.addEventListener('DOMContentLoaded', function(){
+    const form = document.getElementById('deleteBookForm')
+
+    form.addEventListener('submit', function(event) {
+    event.preventDefault(); // Prevent the default form submission
+
+    // Getting the User's input value from the form
+    var title = document.getElementById('deleteBookTitle').value;
+
+    // AJAX call to delete the book
+    fetch('/book/' + encodeURIComponent(title), {
+        method: 'DELETE'
+    })
+    .then(response => {
+        // Redirect to home page on successful deletion
+        if (response.ok) {
+            window.location.href = '/';
+        } else{
+            return response.json();
+        }   
+    })
+    .then(data => {
+        console.log('Success:', data.message); // Handle success 
+    })
+    .catch((error) => {
+        console.error('Error:', error);
+        // Handle error
+    })
+})
+});
